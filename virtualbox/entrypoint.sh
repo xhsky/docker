99cdefd5b22c32eab8e2a1e9c711507eedf0894f
sky <x_hsky@163.com>
2019-12-20 10:06:19 +0800

    update

---
+++ virtualbox/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+groupmod -o -g $AUDIO_GID audio
+groupmod -o -g $VIDEO_GID video
+if [ $GID != $(echo `id -g wechat`) ]; then
+    groupmod -o -g $GID wechat
+fi
+if [ $UID != $(echo `id -u wechat`) ]; then
+    usermod -o -u $UID wechat
+fi
+chown wechat:wechat /WeChatFiles
+
+
+/opt/deepinwine/apps/Deepin-$APP/run.sh
+sleep 3500
+#su wechat <<EOF
+#
+#if [ "$1" ]; then
+#    echo "deepin-wine $1"
+#    deepin-wine $1
+#else
+#    echo "启动 $APP"
+#    "/opt/deepinwine/apps/Deepin-$APP/run.sh"
+#    sleep 3600
+#fi
+#
+#exit 0
+#EOF
